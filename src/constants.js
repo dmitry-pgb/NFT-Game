314970870280204acc229c150bc0fceb8545bc40
dmitry-pgb <dmitrypgb25@gmail.com>
2021-11-26 20:31:17 +0300

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0x64a17F536C18111B3A3B90c2097490F559222C8C';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+
+export { CONTRACT_ADDRESS, transformCharacterData };
